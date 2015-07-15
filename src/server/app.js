cf30ce610ce4308d9976ee364ef34c55f1415585
Raúl López <rldona@gmail.com>
2015-07-15 11:24:35 +0200

    [FEAT] add node server + express

---
+++ src/server/app.js
@@ -0,0 +1,59 @@
+/*jshint node:true*/
+'use strict';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var compress = require('compression');
+var cors = require('cors');
+// var errorHandler = require('./routes/utils/errorHandler')();
+// var favicon = require('serve-favicon');
+var logger = require('morgan');
+var port = process.env.PORT || 7203;
+// var routes;
+
+var environment = process.env.NODE_ENV;
+
+// app.use(favicon(__dirname + '/favicon.ico')); // TODO: add favicon
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(compress());
+app.use(logger('dev'));
+app.use(cors());
+// app.use(errorHandler.init); // TODO: add errorHandler
+
+// routes = require('./routes/index')(app); // TODO: add routes
+
+console.log('=================================');
+console.log('SERVER START');
+console.log('=================================');
+console.log('Port = ' + port);
+console.log('Env = ' + environment);
+console.log('=================================');
+
+app.get('/ping', function(req, res, next) {
+    console.log(req.body);
+    res.send('pong');
+});
+
+switch (environment) {
+    case 'build':
+        console.log('** BUILD **');
+        app.use(express.static('./build/'));
+        app.use('/*', express.static('./build/index.html'));
+        break;
+    default:
+        console.log('Development');
+        console.log('=================================');
+        app.use(express.static('./src/client/'));
+        app.use(express.static('./'));
+        app.use(express.static('./tmp'));
+        app.use('/*', express.static('./src/client/index.html'));
+        break;
+}
+
+app.listen(port, function() {
+    console.log('Express server listening on port ' + port);
+});
