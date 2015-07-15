f78251958e45bd1d0111c7f409ad7225a974e5e8
Raúl López <rldona@gmail.com>
2015-07-15 14:27:07 +0200

    [FEAT] add gulp.config & /core/config.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    config = require('./gulp.config');
 
 gulp.task('default', function() {
     console.log('Hello Gulp !!');
+    console.log(config);
 });
